2aa68e8c5540cf2f88eb4288a39ae448638efa50
Antony Garand <9059487+AntonyGarand@users.noreply.github.com>
2024-11-11 09:23:22 -0500

    Added leaflet images

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // base: 'bsmap.osrs.cloud', 
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./node_modules/leaflet/dist/images/*",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
+});
